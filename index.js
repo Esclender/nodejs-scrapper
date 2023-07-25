5e2336bfdafcdf12d788cecdf9dd6a099b93f36f
Esclender <elesclenderlugo@gmail.com>
2023-07-24 20:57:02 -0500

    scrapper v01: attributes selecter

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ const infoArr = [
   {
     tag: '.offer-card-title',
     attr: 'Title'
+  },
+  {
+    tag: '.offer-card-image-main',
+    attr: 'Image',
+    src: async (offer) => {
+      return await offer.evaluate(node => node.src)
+    }
   }
 ]
 
@@ -20,9 +27,16 @@ const getOfferOBj = async (page, infos) => {
     const offer = await page.locator(info.tag).all()
 
     for (let i = 0; i < offer.length; i++) {
-      obj[i] = {
-        ...obj[i],
-        [info.attr]: await offer[i].textContent()
+      if (!info.src) {
+        obj[i] = {
+          ...obj[i],
+          [info.attr]: await offer[i].textContent()
+        }
+      } else {
+        obj[i] = {
+          ...obj[i],
+          [info.attr]: await info.src(offer[i])
+        }
       }
     }
   }
@@ -34,8 +48,9 @@ const getOfferOBj = async (page, infos) => {
   const browser = await chromium.launch()
   const page = await browser.newPage()
   await page.goto(url)
-
   console.log(await getOfferOBj(page, infoArr))
 
   await browser.close()
 })()
+
+/* console.log(await (await offer[i].evaluate(node => node.src)).jsonValue()) */
